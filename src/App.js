761c107ebb3917d1cc1da59fea8743d69f66847c
J2kwes <31591236+J2kwes@users.noreply.github.com>
2020-02-15 22:15:06 +0100

    Added the header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+<<<<<<< Updated upstream
 
 const HatsPage = () => {
   return (
@@ -12,14 +13,27 @@ const HatsPage = () => {
     </div>
   );
 };
+=======
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop/hats" component={HatsPage} />
       </Switch>
+=======
+      <Header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact="true" path="/shop" component={ShopPage} />
+        </Switch>
+      </Header>
+>>>>>>> Stashed changes
     </div>
   );
 }
